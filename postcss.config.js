c0cacaf6aa25df50b324f288184cb4bc3c5475c2
Shaun Drong <sdrong@uceap.universityofcalifornia.edu>
2022-03-21 13:27:22 -0700

    work toward upgrading postcss processing

--- postcss.config.js
+++ postcss.config.js
@@ -8,11 +8,16 @@ const tailwindcss = require('tailwindcss');
 const hexrgba = require('postcss-hexrgba');
 const stylelint = require('stylelint');
 const postcssReporter = require('postcss-reporter');
+const postcssInlineSvg = require('postcss-inline-svg');
+const postcssSvgo = require('postcss-svgo');
+const postcssUtils = require('postcss-utilities');
 
 module.exports = ({ options, env }) => {
   return {
     plugins: [
       stylelint(),
+      postcssUtils(),
+      postcssInlineSvg(),
       // tailwindConfig is set per *design system* webpack.config.js.
       options.postcssOptions && options.postcssOptions.tailwindConfig && tailwindcss(options.postcssOptions.tailwindConfig),
       // Hex in rgba like Sass
@@ -21,6 +26,8 @@ module.exports = ({ options, env }) => {
       postcssPresetEnv(),
       // Heavy processing for production
       env === 'production' && cssnano(),
+      // else
+      env !== 'production' && postcssSvgo(),
       postcssReporter({ clearReportedMessages: true }),
     ],
   };
